e7a630badf2e763e5fbde0736faf57b45eb0ccbd
vasundra-172 <vishwakarmavasundra@gmail.com>
2024-12-05 23:48:02 +0530

    cursor changes

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Pagination({handlePrev, handleNext, pageNo}) {
   return (
     <div className='bg-gray-400 p-4 mt-8 flex justify-center'>
-        <div onClick={handlePrev} className='px-8'><i className="fa-solid fa-arrow-left"></i></div>
+        <div onClick={handlePrev} className='px-8 hover:cursor-pointer'><i className="fa-solid fa-arrow-left"></i></div>
         <div className='font-bold'>{pageNo}</div>
-        <div onClick={handleNext} className='px-8'><i className="fa-solid fa-arrow-right"></i></div>
+        <div onClick={handleNext} className='px-8 hover:cursor-pointer'><i className="fa-solid fa-arrow-right"></i></div>
     </div>
   )
 }
